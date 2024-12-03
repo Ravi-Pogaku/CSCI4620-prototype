105901d3f4679ffb492acc796b77bc14036f05b4
gowtham <gowtham.rajendra@ontariotechu.net>
2024-12-03 02:02:51 -0500

    created workout page + added routes

--- hci-prototype/src/App.jsx
+++ hci-prototype/src/App.jsx
@@ -1,10 +1,18 @@
+import {Routes, Route} from 'react-router-dom'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Home from './pages/Home'
+import Workouts from './pages/Workouts'
 
 
 function App() {
   return (
-    <Home/>
+    <div className='d-flex justify-content-center vw-100'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/workouts' element={<Workouts />} />
+      </Routes>
+    </div>
   )
 }
 
---
+++ hci-prototype/src/components/WorkoutComponent.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import { useState } from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import '../styles/workout.css'
+
+
+export default function WorkoutComponent ({workoutName, duration, icon}) {
+    const [isPressed, setIsPressed] = useState(false);
+    const [isDragging, setIsDragging] = useState(false);
+
+    // we pressed on the card and we are not dragging
+    const handleMouseDown = () => { 
+        setIsPressed(true);
+        setIsDragging(false);
+    };
+
+    // we are moving the mouse 
+    // if we are pressing the card, we are dragging it
+    const handleMouseMove = () => {
+        if (isPressed) {
+            setIsDragging(true);
+            setIsPressed(false);
+        }
+    };
+
+    // we released the mouse button
+    // if we are not dragging, we are not pressing the card anymore
+    const handleMouseUp = () => {
+        if (!isDragging) {
+            setIsPressed(false);
+        }
+    };
+
+    return (
+        <Card className={`d-flex align-items-center justify-content-between p-2 vw-80 ${isPressed ? 'pressed' : ''}`}
+        style={{
+            backgroundColor: '#3F6634',
+            color: '#ffffff',
+            height: '70px',
+            marginLeft: '10px',
+            marginRight: '10px',
+            marginBottom: '2px',
+            // borderRadius: '10%', 
+            }}
+            onMouseDown={handleMouseDown}
+            onMouseMove={handleMouseMove}
+            onMouseUp={handleMouseUp}
+            onMouseLeave={handleMouseUp} 
+        >
+            <div className="d-flex align-items-center">
+                <FontAwesomeIcon icon={icon} size="2x" />
+                <div className="ms-2">
+                    <h6 className="mb-0">{workoutName}</h6>
+                    <p className="mb-0">
+                        <small>Duration: {duration} minutes</small>
+                    </p>
+                </div>
+            </div>
+        </Card>
+
+    );
+};
