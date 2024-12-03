ab0c9af59167ac96b991aeb54afe84b1f7f8bf60
Nimra-R <114171010+Nimra-R@users.noreply.github.com>
2024-12-03 10:22:12 -0500

    Update App.jsx
    added AIAssistant.jsx file

--- hci-prototype/src/App.jsx
+++ hci-prototype/src/App.jsx
@@ -1,11 +1,24 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Home from './pages/Home'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './pages/Home';
+import AIAssistant from './pages/AIAssistant';
 
 function App() {
+  const currentPath = window.location.pathname; // Get the current URL path
+
+  const renderPage = () => {
+    switch (currentPath) {
+      case '/AIAssistant.jsx': // If the path is /AIAssistant.jsx, render AIAssistant
+        return <AIAssistant />;
+      default: // Default to Home for any other path
+        return <Home />;
+    }
+  };
+
   return (
-    <Home/>
-  )
+    <div>
+      {renderPage()}
+    </div>
+  );
 }
 
-export default App
+export default App;
